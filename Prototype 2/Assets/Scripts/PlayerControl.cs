4929f8628960671152892f250a7a83a3a7f21ee1
Fatih Ozdil <muhammedfatihozdil@gmail.com>
2022-11-26 21:06:59 +0300

    add left and right boundary for the player

--- Prototype 2/Assets/Scripts/PlayerControl.cs
+++ Prototype 2/Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerControl : MonoBehaviour
 {
     private float horizontalInput;
     private float speed = 25.0f;
+    private float boundary = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,15 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position;
+        if (transform.position.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, pos.y, pos.z);
+        }
+        else if (transform.position.x > boundary)
+        {
+            transform.position = new Vector3(boundary, pos.y, pos.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
