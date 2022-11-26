e623d1bb956e2e626113b259f4aefcf59ef3886e
Fatih Ozdil <muhammedfatihozdil@gmail.com>
2022-11-26 21:42:16 +0300

    make animals into prefabs

--- Prototype 2/Assets/Scripts/MoveForward.cs
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    private float speed = 40f; 
+    public float speed = 40f; 
     // Start is called before the first frame update
     void Start()
     {
